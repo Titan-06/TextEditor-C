5f7a0eeff27bc121741b262d2cdb99469f309dd7
Syed Alkama <syedalkama2419@gmail.com>
2026-08-16 19:50:40 +0000

    Added find functionality

--- kilo.c
+++ kilo.c
@@ -49,7 +49,7 @@ struct editorConfig E;
 /* Prototypes */
 void editorSetStatusMessage(const char *fmt, ...);
 void editorRefreshScreen();
-char *editorPrompt(char *prompt);
+char *editorPrompt(char *prompt, void (*callback)(char *, int));
 /* Defines*/
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define KILO_VERSION "0.0.1"
@@ -239,6 +239,24 @@ int editorCxtoRx(erow *row, int cx)
         rx++;
     }
     return rx;
+}
+
+int editorRxtoCx(erow *row, int rx)
+{
+    int cur_rx = 0;
+    int cx;
+    for (cx = 0; cx < row->size; cx++)
+    {
+        if (row->chars[cx] == '\t')
+        {
+            cur_rx += (KILO_TAB_STOP - 1) - (cur_rx % KILO_TAB_STOP);
+        }
+        cur_rx++;
+
+        if (cur_rx > rx)
+            return cx;
+    }
+    return cx;
 }
 
 void editorUpdateRow(erow *row)
@@ -438,7 +456,7 @@ void editorOpen(char *filename)
 void editorSave()
 {
     if (E.filename == NULL)
-        E.filename = editorPrompt("Save as: %s (ESC TO CANCEL)");
+        E.filename = editorPrompt("Save as: %s (ESC TO CANCEL)", NULL);
     if (E.filename == NULL)
     {
         editorSetStatusMessage("Save aborted");
@@ -469,9 +487,80 @@ void editorSave()
     editorSetStatusMessage("Can't Write on the file! : %s", strerror(errno));
 }
 
+/* find */
+
+void editorFindCallback(char *query, int key)
+{
+    static int last_match = -1;
+    static int direction = 1;
+
+    if (key == '\r' || key == '\x1b')
+    {
+        last_match = -1;
+        direction = 1;
+        return;
+    }
+    else if (key == ARROW_RIGHT || key == ARROW_DOWN)
+    {
+        direction = 1;
+    }
+    else if (key == ARROW_LEFT || key == ARROW_UP)
+    {
+        direction = -1;
+    }
+    else
+    {
+        last_match = -1;
+        direction = 1;
+    }
+
+    if (last_match == -1)
+        direction = 1;
+    int current = last_match;
+    for (int i = 0; i < E.numrows; i++)
+    {
+        current += direction;
+        if (current == -1)
+            current = E.numrows - 1;
+        else if (current == E.numrows)
+            current = 0;
+
+        erow *row = &E.row[current];
+        char *match = strstr(row->render, query);
+        if (match)
+        {
+            last_match = current;
+            E.cy = current;
+            E.cx = editorRxtoCx(row, match - row->render);
+            E.rowoff = E.numrows;
+            break;
+        }
+    }
+}
+
+void editorFind()
+{
+    int saved_cx = E.cx;
+    int saved_cy = E.cy;
+    int saved_coloff = E.coloff;
+    int saved_rowoff = E.rowoff;
+    char *query = editorPrompt("Search: %s (PRESS ESC TO CANCEL / Enter and Arrow to search)", editorFindCallback);
+    if (query)
+    {
+        free(query);
+    }
+    else
+    {
+        E.cx = saved_cx;
+        E.cy = saved_cy;
+        E.coloff = saved_coloff;
+        E.rowoff = saved_rowoff;
+    }
+}
+
 /* Handles Input */
 
-char *editorPrompt(char *prompt)
+char *editorPrompt(char *prompt, void (*callback)(char *, int))
 {
     size_t bufsize = 120;
     char *buf = malloc(bufsize);
@@ -494,6 +583,8 @@ char *editorPrompt(char *prompt)
         else if (c == '\x1b')
         {
             editorSetStatusMessage("");
+            if (callback)
+                callback(buf, c);
             free(buf);
             return NULL;
         }
@@ -502,6 +593,8 @@ char *editorPrompt(char *prompt)
             if (buflen != 0)
             {
                 editorSetStatusMessage("");
+                if (callback)
+                    callback(buf, c);
                 return buf;
             }
         }
@@ -515,6 +608,8 @@ char *editorPrompt(char *prompt)
             buf[buflen++] = c;
             buf[buflen] = '\0';
         }
+        if (callback)
+            callback(buf, c);
     }
 }
 
@@ -613,6 +708,9 @@ void editorProcessKeyPress()
         {
             E.cx = E.row[E.cy].size;
         }
+        break;
+    case CTRL_KEY('f'):
+        editorFind();
         break;
     case BACKSPACE:
     case CTRL_KEY('h'):
